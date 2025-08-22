c5e65da71de533776bf60b2065678e757af72ffa
ABHINAV SINGH <abhinavsi434@gmail.com>
2025-08-22 23:19:04 +0530

    commit

--- DjangoCurd/employee/models.py
+++ DjangoCurd/employee/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=50)
+    employee_name = models.CharField(max_length=100)
+    employee_email = models.EmailField()
+    employee_contact = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.employee_name
--- DjangoCurd/employee/views.py
+++ DjangoCurd/employee/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .forms import EmployeeForm
+from django.shortcuts import redirect
 # Create your views here.
+def create_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+        else:
+            form = EmployeeForm()
+        return render(request, 'create.html', {'form' : form})
+    
